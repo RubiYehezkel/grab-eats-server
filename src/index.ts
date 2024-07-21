13a0be0199bcd87ae6080bddf90f2ad7fd530797
Rubi Yehezkel <rubke08@gmail.com>
2024-07-21 16:52:51 +0300

    added checkout flow to stripe

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import "dotenv/config";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 dotenv.config();
 const PORT_NUMBER = 7000;
@@ -21,8 +22,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "Health OK" });
@@ -31,6 +33,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(PORT_NUMBER, () => {
   console.log(`server running on port: ${PORT_NUMBER}`);
