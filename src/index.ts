8061bcb5a64a0c8b5444c470244ac73dadcf9e68
Rubi Yehezkel <rubke08@gmail.com>
2024-07-31 13:13:34 +0300

    fixed cors error after deployment

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,18 @@ cloudinary.config({
 });
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorizaion"
+  );
+  next();
+});
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 app.use(express.json());
 
